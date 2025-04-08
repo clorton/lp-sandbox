53c205078d0468e50dba32630b384026b6481b00
Christopher Lorton <clorton@gatesfoundation.org>
2025-04-08 16:05:37 -0700

    Fix version number

--- src/laser_polio/__init__.py
+++ src/laser_polio/__init__.py
@@ -2,10 +2,15 @@ from .distributions import *  # noqa F403
 from .pars import *  # noqa F403
 from .model import *  # noqa F403
 from pathlib import Path
+from importlib.metadata import version, PackageNotFoundError
 
 # from .seir_mpm import *
 from .utils import *  # noqa F403
 
-__version__ = "0.1"
+try:
+    __version__ = version("laser-polio")
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = "unknown"
 
 root = Path(__file__).resolve().parents[2]
