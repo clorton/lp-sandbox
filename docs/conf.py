fe8b51de75f31e832112450dabe33bdb2d1e9e90
clorton <clorton@users.noreply.github.com>
2025-04-16 21:13:49 +0000

    Bump version: 1.0.4 → 1.0.5

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ project = "LASER Polio (PHASER)"
 year = "2025"
 author = "Institute for Disease Modeling"
 copyright = f"{year}, Gates Foundation"
-version = release = "1.0.4"
+version = release = "1.0.5"
 
 pygments_style = "trac"
 templates_path = ["."]
